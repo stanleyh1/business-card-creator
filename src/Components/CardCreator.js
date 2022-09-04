1ede169f0b46cdc4087cf33f74d2b3c23469a549
Hannah Stanley <hannahstanley63@gmail.com>
2022-09-04 20:28:01 +0100

    tailwind css started

--- src/Components/CardCreator.js
+++ src/Components/CardCreator.js
@@ -26,21 +26,21 @@ const CardCreator = () => {
 
     return(
         <>
-        <h1>Create your business card...</h1>
-        <form className='business-card-form'>
+        <h1 className="">Create your business card...</h1>
+        <form className="">
         <legend>Your name</legend>
-        <input className="name" placeholder="Your name here" name="name" value={businessCard.name} onChange={handleChange}/>
+        <input className="border-gray-300 px-3 py-2 shadow-sm rounded-lg focus:outline-none focus:border-indigo-500" placeholder="Your name here" name="name" value={businessCard.name} onChange={handleChange}/>
         <legend>Business Name</legend>
-        <input className='business-name' placeholder='Business name here' name="businessName" value={businessCard.businessName} onChange={handleChange}/>
+        <input className='' placeholder='Business name here' name="businessName" value={businessCard.businessName} onChange={handleChange}/>
         <legend>Job title</legend>
-        <input className='job-title' placeholder='Your job title here' name="jobTitle" value={businessCard.jobTitle} onChange={handleChange}/>
+        <input className='' placeholder='Your job title here' name="jobTitle" value={businessCard.jobTitle} onChange={handleChange}/>
         <legend>Phone number</legend>
-        <input className='phone-numer' placeholder='Your phone number here' name="phoneNumber" value={businessCard.phoneNumber} onChange={handleChange}/>
+        <input className='' placeholder='Your phone number here' name="phoneNumber" value={businessCard.phoneNumber} onChange={handleChange}/>
         <legend>Email address</legend>
-        <input className='email-address' placeholder='Your email address here' name="email" value={businessCard.email} onChange={handleChange}/>
+        <input className='' placeholder='Your email address here' name="email" value={businessCard.email} onChange={handleChange}/>
         <legend>Website</legend>
-        <input className='website' placeholder='Your website URL here' name="website" value={businessCard.website} onChange={handleChange}/>
-        <button onClick={createCard}>Create your business card!</button>
+        <input className='' placeholder='Your website URL here' name="website" value={businessCard.website} onChange={handleChange}/>
+        <button className="" onClick={createCard}>Create your business card!</button>
         </form>
         </>
     )
